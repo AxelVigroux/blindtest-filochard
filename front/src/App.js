7fcd7d8dc5798bb57fdd87f1157f3bbd26a4c420
AxelVigroux <vix.ax@hotmail.fr>
2022-04-06 08:06:31 +0400

    done

--- front/src/App.js
+++ front/src/App.js
@@ -1,28 +1,21 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
-<<<<<<< HEAD
 import ReactPlayer from "react-player";
 import Logo from "./assets/logo_le_filochard.png";
-=======
->>>>>>> parent of cdd70c9 (everything it's working, go css now !)
+import Icon from "./assets/logo_icon_le_filochard.png";
+import Lazare from "./assets/lazareDanse_320x650.gif";
+import Verre from "./assets/Verre.gif";
 
 function App() {
   const [playListId, setPlaylistId] = useState("");
   const [fetchedSongs, setFetchedSongs] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
   const [index, setIndex] = useState(0);
   const [ready, setReady] = useState(false);
   const [show, setShow] = useState(false);
+  const [playing, setPlaying] = useState(false);
+  const [rotate, setRotate] = useState(false);
 
-<<<<<<< HEAD
-  const playPause = () => {
-    setPlaying(!playing);
-  };
-
-  console.log(fetchedSongs);
-=======
   const nextSong = () => {
     if (index + 1 >= fetchedSongs.length) {
       setIndex(0);
@@ -43,10 +36,10 @@ function App() {
     setShow(!show);
   };
 
-  console.log({ fetchedSongs });
->>>>>>> parent of cdd70c9 (everything it's working, go css now !)
-
-  // const url = "https://www.youtube.com/embed/";
+  const playPause = () => {
+    setPlaying(!playing);
+    setRotate(!rotate);
+  };
 
   useEffect(() => {
     axios
@@ -57,24 +50,25 @@ function App() {
           process.env.REACT_APP_YOUTUBE_API_KEY
       )
       .then((response) => {
-        createSongs(response.data.items);
+        setFetchedSongs(response.data.items);
+        setReady(true);
       })
       .catch((err) => {
-        setError(err);
         console.error(err);
       });
   }, [playListId]);
 
-  console.log("SONGS", { fetchedSongs });
-
   return (
     <div className="App">
       <div className="main-title">
-        <img src={Logo} alt="logo du bar le filochard" />
+        <img src={Logo} alt="Nom du bar" className="logo" />
+        <h3>Quizz musical bruyant et bordélique !</h3>
       </div>
       {ready === false ? (
         <div className="input-form">
+          <h2>Copie colle l'id de la playlist</h2>
           <input
+            placeholder="Id de la playlist ici !"
             type="text"
             name="url"
             onChange={(e) => {
@@ -85,14 +79,55 @@ function App() {
         </div>
       ) : (
         <div className="main-content">
-          <h2>Chanson numéro {index}</h2>
+          {" "}
+          <img className="verre" src={Verre} alt="Un bon verre" />
+          <img className="lazare" src={Lazare} alt="Lazare qui danse" />
+          {rotate ? (
+            <img
+              src={Icon}
+              className="icon rotating"
+              alt="mascotte du filochard"
+            />
+          ) : (
+            <img className="icon" src={Icon} alt="Tête du logo du Filochard" />
+          )}
           <div className="current-song">
-            <button onClick={prevSong}>Précédent</button>
-            <button onClick={toggle}>
-              {show ? "Cacher le titre" : "Voir le titre"}
-            </button>
-            {show ? fetchedSongs[index].snippet.title : null}
-            <button onClick={nextSong}>Suivant</button>
+            <h2 className="song-number">
+              Chanson numéro {index + 1} / {fetchedSongs.length}
+            </h2>
+            <div className="text-content">
+              <button className="reveal-button" onClick={toggle}>
+                {show ? "Cacher le titre" : "Afficher le titre"}
+              </button>
+              {show ? (
+                <h2 className="title">{fetchedSongs[index].snippet.title} </h2>
+              ) : null}
+              <ReactPlayer
+                className="player"
+                url={
+                  "https://www.youtube.com/watch?v=" +
+                  `${fetchedSongs[index].contentDetails.videoId}`
+                }
+                volume={1}
+                controls={true}
+                height="50%"
+                width="50%"
+                playing={playing}
+              ></ReactPlayer>
+            </div>
+            <div className="controllers">
+              <button className="prev-button" onClick={prevSong}>
+                Précédent
+              </button>
+              {playing ? (
+                <button onClick={playPause}>Pause</button>
+              ) : (
+                <button onClick={playPause}>Play</button>
+              )}
+              <button className="next-button" onClick={nextSong}>
+                Suivant
+              </button>
+            </div>
           </div>
         </div>
       )}
